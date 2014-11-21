7a3bdee0ca5461a68b92fc4965f6a0d087446185
Joseph Lust <lifeoflust@gmail.com>
2014-11-20 23:22:14 -0500

    * Updated to BigInteger. Works much better. 1.1s for iterative, 0.4s for Fermat!

--- datastructures/src/main/java/com/github/twistedpair/math/Primer.java
+++ datastructures/src/main/java/com/github/twistedpair/math/Primer.java
@@ -1,7 +1,9 @@
 package com.github.twistedpair.math;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Various fun with prime numbers
@@ -11,18 +13,21 @@ import java.util.List;
  */
 public final class Primer {
 
+	final Random rand = new Random(); // seed for double the fun!
+
 	/**
 	 * Use Fermat if number is >= limit
 	 */
-	private static final long FERMAT_LIMIT = 100;
-	private static final long FERMAT_ITERATIONS = 200; // 1-1/2^k confidence
+	private static final long FERMAT_LIMIT = 1_000_000_000L;
+	private static final long FERMAT_ITERATIONS = 100; // 1-1/2^k confidence
+	private static final BigInteger ONE = BigInteger.ONE;
 
 	/**
 	 * Find the primes up to this value
 	 * @param limit
 	 * @return
 	 */
-	public static final Long[] findPrimes(final long limit) {
+	public final Long[] findPrimes(final long limit) {
 		final List<Long> primes = new ArrayList<>(100);
 
 		if (limit >= 2) {
@@ -41,19 +46,19 @@ public final class Primer {
 
 	// TODO hack: BigInteger.isProbablePrime();
 
-	public static final boolean isPrime(final long p) {
+	public final boolean isPrime(final long p) {
 
 		// quick sanity checks
-		if(p < 2) { return false;}        // bounds check
-		if(p == 2) { return true;}        // only even prime check
-		if(p % 2 ==0 && p!=2) { return false;}   // even check
+		if (p < 2) { return false; } // bounds check
+		if (p == 2) { return true; } // only even prime check
+		if (p % 2 == 0) { return false; } // even check
 
 		// big number? could take a while, branch logic
-		return p < FERMAT_LIMIT ? isIterativePrime(p) : isFermatPrime(p);
+		return p < FERMAT_LIMIT ? isIterativePrime(p) : isFermatPrime(BigInteger.valueOf(p));
 	}
 
-	private static boolean isIterativePrime(final long p) {
-		// iterative approach
+	private boolean isIterativePrime(final long p) {
+		// iterative approach - small numbers, try floats (rounding anyway)
 		final long limit = (int) Math.ceil(Math.sqrt(p));
 		for (long i = 3; i <= limit; i += 2) {
 			if(p % i==0 ) {
@@ -63,25 +68,28 @@ public final class Primer {
 		return true; // no divisors found
 	}
 
-	private static boolean isFermatPrime(final long p) {
-
+	/**
+	 * Apply Fermat prime test.<br>
+	 * Fermat witness or liar? 1/2^k chance we're wrong for k passes
+	 * 
+	 * @see http://en.wikipedia.org/wiki/Fermat_primality_test
+	 * @param p
+	 * @return
+	 */
+	private boolean isFermatPrime(final BigInteger p) {
 		long i = 1;
 		while( i < FERMAT_ITERATIONS) {
-			// Fermat witness or liar? 1/2^k chance we're wrong for k passes
-			// need a = [1,p)
-			// http://en.wikipedia.org/wiki/Fermat_primality_test
-			/*
-			long a;
-			while(true) {
-				a = (long) Math.floor(Math.random()*(p-1))+1; // [0,1) * p-1 => [0,p-1) +1 => [1,p) for p>1
-				if (a > 1) {
+			// ugly high quality random, misses ~50% of time, no bounded random for BigInt
+			BigInteger a;
+			while (true) {
+				a = new BigInteger(p.bitLength(), rand);
+				if (ONE.compareTo(a) <= 0 && a.compareTo(p) < 0) {
 					break;
 				}
 			}
-			 */
-			final long a = (long) Math.floor(Math.random()*(p-3))+2; // [0,1) * p-1 => [0,p-1) +1 => [1,p) for p>1
-			final boolean notPrime = Math.pow(a, p - 1) % p == 1; // arr, comparing ints & floating point!
-			if(notPrime) {
+			// test
+			final BigInteger ferm = a.modPow(p.subtract(ONE), p); // (1,p)
+			if (!ferm.equals(ONE)) {
 				return false;
 			}
 			i++;
--- datastructures/src/test/java/com/github/twistedpair/math/PrimerTest.java
+++ datastructures/src/test/java/com/github/twistedpair/math/PrimerTest.java
@@ -2,8 +2,6 @@ package com.github.twistedpair.math;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
-
 import org.junit.Test;
 
 
@@ -14,58 +12,57 @@ import org.junit.Test;
  */
 public final class PrimerTest {
 
+	final Primer primer = new Primer();
+
 	@Test
 	public void testPrimerangeTwoOrLess() {
-
-		assertEquals(0, Primer.findPrimes(0).length);
-		assertEquals(0, Primer.findPrimes(1).length);
-		assertEquals(1, Primer.findPrimes(2).length);
+		assertEquals(0, primer.findPrimes(0).length);
+		assertEquals(0, primer.findPrimes(1).length);
+		assertEquals(1, primer.findPrimes(2).length);
 	}
 
 	@Test
-	public void testPrimerangeOverOneHundred() {
-		final Long[] primes = Primer.findPrimes(100);
+	public void testPrimerangeOneHundred() {
+		final Long[] primes = primer.findPrimes(100);
 		assertEquals(25, primes.length);
-		System.out.println(Arrays.toString(primes));
 	}
 
 	@Test
 	public void testPrimerangeOneThousand() {
-		final Long[] primes = Primer.findPrimes(1000);
+		final Long[] primes = primer.findPrimes(1000);
 		assertEquals(168, primes.length);
-		System.out.println(Arrays.toString(primes));
 	}
 
 	@Test
 	public void testPrimerangeTenThousand() {
-		final Long[] primes = Primer.findPrimes(10000);
-		assertEquals(168, primes.length);
-		System.out.println(Arrays.toString(primes));
+		final Long[] primes = primer.findPrimes(10000);
+		assertEquals(1229, primes.length);
 	}
 
 	@Test
 	public void testPrime() {
 
-		assertEquals("bad result 0", false, Primer.isPrime(0));
-		assertEquals("bad result 1", false, Primer.isPrime(1));
-		assertEquals("bad result 2", true, Primer.isPrime(2));
-		assertEquals("bad result 3", true, Primer.isPrime(3));
-		assertEquals("bad result 4", false, Primer.isPrime(4));
-		assertEquals("bad result 5", true, Primer.isPrime(5));
-		assertEquals("bad result 6", false, Primer.isPrime(6));
-		assertEquals("bad result 7", true, Primer.isPrime(7));
-		assertEquals("bad result 8", false, Primer.isPrime(8));
-		assertEquals("bad result 9", false, Primer.isPrime(9));
-		assertEquals("bad result 17", true, Primer.isPrime(17));
-		assertEquals("bad result 53", true, Primer.isPrime(5));
+		assertEquals("bad result 0", false, primer.isPrime(0));
+		assertEquals("bad result 1", false, primer.isPrime(1));
+		assertEquals("bad result 2", true, primer.isPrime(2));
+		assertEquals("bad result 3", true, primer.isPrime(3));
+		assertEquals("bad result 4", false, primer.isPrime(4));
+		assertEquals("bad result 5", true, primer.isPrime(5));
+		assertEquals("bad result 6", false, primer.isPrime(6));
+		assertEquals("bad result 7", true, primer.isPrime(7));
+		assertEquals("bad result 8", false, primer.isPrime(8));
+		assertEquals("bad result 9", false, primer.isPrime(9));
+		assertEquals("bad result 17", true, primer.isPrime(17));
+		assertEquals("bad result 53", true, primer.isPrime(5));
 
 		int n = 1;
 		while (n < 1000) { // 3000 tests
-			assertEquals("bad result 1000002", false, Primer.isPrime(1000002));
-			assertEquals("bad result 1000003", true, Primer.isPrime(1000003));
-			assertEquals("bad result 1000004", false, Primer.isPrime(1000004));
-			assertEquals("bad result 179426549", true,
-					Primer.isPrime(179426549));
+			assertEquals("bad result 1000002", false, primer.isPrime(1000002L));
+			assertEquals("bad result 1000003", true, primer.isPrime(1000003L));
+			assertEquals("bad result 1000004", false, primer.isPrime(1000004L));
+			assertEquals("bad result 32416190071L", true, primer.isPrime(32416190071L));
+			assertEquals("bad result 179426549", true, primer.isPrime(179426549L));
+			assertEquals("bad result 9223372036854775807L", false, primer.isPrime(9223372036854775807L));
 			n++;
 		}
 	}
